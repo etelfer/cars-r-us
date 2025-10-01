0c6642d85534f56370dfcbfaffc96e61fdf19313
Erin Telfer <eringtelfer@gmail.com>
2025-09-30 21:24:46 -0500

    added paintOptions functions code to define it as well as event listener to handle Paint Color Choice. will do for all other modules on saturday.

--- scripts/paintColors.js
+++ scripts/paintColors.js
@@ -1,3 +1,19 @@
 export const PaintOptions = async () => {
-    
+    const response = await fetch("http://localhost:8088/paintColors")
+    const paintColors = await response.json()
+
+        document.addEventListener("change", handlePaintColorChoice)
+
+    const paintColorsHTML = paintColors.map((paintColor => {
+        return `<input type="radio" name="paintColor" value=${paintColor.id} /> ${paintColor.color}`
+    })
+)
+    return paintColorsHTML.join("")
+}
+
+const handlePaintColorChoice = (event) => {
+
+    if (event.target.name === "paintColor") {
+        setStyleChoice(event.target.value)
+    }
 }
